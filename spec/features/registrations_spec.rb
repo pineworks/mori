76bd090b2db6a0ec1371a923bc99500888b0cf06
Aaron Miler <aaron@aaronmiler.com>
2014-02-22 20:16:02 -0800

    Moved forms to partials, hopefully improved coverage

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -20,4 +20,13 @@ describe "The registration process", :type => :feature do
     visit '/sign_up'
     current_path.should eq Mori.configuration.after_signup_url
   end
+  it "should not allow invalid credentials" do
+    visit '/sign_up'
+    within('#new_mori_user') do
+      fill_in 'Email', :with => "namenamename"
+      fill_in 'Password', :with => "imapassword123"
+    end
+    click_button 'Create User'
+    current_path.should eq '/registrations'
+  end
 end
