7bca1e9731c2a57175e29d3b3e8c4dfb1698c6a2
Aaron Miler <aaron@aaronmiler.com>
2014-04-16 21:00:01 -0700

    Shortened params in passwords controller

--- app/controllers/mori/passwords_controller.rb
+++ app/controllers/mori/passwords_controller.rb
@@ -30,13 +30,13 @@ class Mori::PasswordsController < MoriController
     end
   end
   def reset_password
-    valid, message = Mori::User.reset_password(params[:mori_user][:password_reset_token],params[:mori_user][:password], params[:mori_user][:password_confirmation])
+    valid, message = Mori::User.reset_password(user_params[:password_reset_token],user_params[:password], user_params[:password_confirmation])
     if valid
       warden.authenticate!
       redirect_to Mori.configuration.after_login_url, notice: t('flashes.password_has_been_reset')
     else
       flash[:notice] = message
-      redirect_to "/passwords/reset?token=#{params[:mori_user][:password_reset_token]}"
+      redirect_to "/passwords/reset?token=#{user_params[:password_reset_token]}"
     end
   end
 end
