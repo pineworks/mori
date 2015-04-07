b4b6f423b7fcb7cc107469afc584b020ac0cf775
Aaron Miler <aaron@aaronmiler.com>
2015-04-06 19:29:40 -0700

    Fix a lame bug

--- lib/mori/controller.rb
+++ lib/mori/controller.rb
@@ -11,7 +11,7 @@ module Mori
     end
 
     def signed_in?
-      current_user.preset?
+      current_user.present?
     end
 
     def current_user
--- lib/mori/version.rb
+++ lib/mori/version.rb
@@ -1,3 +1,3 @@
 module Mori
-  VERSION = '0.1.6'
+  VERSION = '0.1.7'
 end
